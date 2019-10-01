1a7fd968bba3ae51d062685a6bef6b211836dc18
Sam <samlee405@yahoo.com>
2019-09-30 22:57:09 -0700

    implemented condition scraping

--- data/webScraper.py
+++ data/webScraper.py
@@ -8,10 +8,11 @@ def getItemStats(url):
 
     # get and clean data
     name = soup.find('h1', attrs = {'class': 'ak-return-link'}).text.strip()
+    equipmentType = soup.find('div', attrs = {'class': 'ak-encyclo-detail-type col-xs-6'}).text[7:]
     lvl = soup.find('div', attrs = {'class': 'ak-encyclo-detail-level col-xs-6 text-right'}).text[7:]
     image = soup.find('img', attrs = {'class': 'img-maxresponsive'})["src"]
-    equipmentType = soup.find('div', attrs = {'class': 'ak-encyclo-detail-type col-xs-6'}).text[7:]
 
+    # Retrieve item stats
     rawStats = soup.find('div', {'class': 'ak-container ak-content-list ak-displaymode-col'})
     stats = {}
 
@@ -20,6 +21,10 @@ def getItemStats(url):
         type = ""
         maxStat = ""
 
+        # check and adjust for the description type that substitutes "HP" for "initiative"
+        description = description.replace("HP", "Initiative")
+
+        # check and adjust for values that have ranges and negative values
         if "to" in description and "-" not in description:
             arr = description.split(' ')
             maxStat = arr[2].replace('%', '')
@@ -45,7 +50,19 @@ def getItemStats(url):
 
         stats[type] = {'description': description, 'maxStat': maxStat}
 
-    item = {'name': name, 'equipment type': equipmentType, 'lvl': lvl, 'image': image, 'stats': stats}
+    # Retrive item conditions
+    rawConditions = soup.find('div', attrs = {'class': 'ak-container ak-panel no-padding'})
+    conditions = {}
+
+    if rawConditions:
+        rawConditions = rawConditions.text.strip().strip("Conditions").strip().split(" ")
+        conditionType = rawConditions[0]
+        conditionLimitType = rawConditions[1]
+        conditionLimit = rawConditions[2]
+
+        conditions = {"conditionType": conditionType, "conditionLimitType": conditionLimitType, "conditionLimit": conditionLimit}
+
+    item = {'name': name, 'equipment type': equipmentType, 'lvl': lvl, 'image': image, 'stats': stats, 'conditions': conditions}
 
     return item
 
@@ -85,6 +102,6 @@ def __main__(lastPage):
 lastPage = 1
 # __main__(lastPage)
 
-testurl = "https://www.dofus.com/en/mmorpg/encyclopedia/equipment/14076-count-harebourg"
+testurl = "https://www.dofus.com/en/mmorpg/encyclopedia/equipment/14162-epeni-belt"
 testdata = getItemStats(testurl)
 writeToFile(testdata)
