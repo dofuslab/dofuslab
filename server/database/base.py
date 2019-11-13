b801c9271b0b5c6de22be7c3a4d4792ea192cd8b
Sam <samlee405@yahoo.com>
2019-11-13 00:54:22 -0800

    migrated from sqlite to postgres

--- server/database/base.py
+++ server/database/base.py
@@ -3,9 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
-db_name = 'database.db'
-db_path = os.path.join(os.path.dirname(__file__), db_name)
-db_uri = 'sqlite:///{}'.format(db_path)
+db_uri = 'postgres+psycopg2://postgres:password@localhost:5432/dofus_sets'
 engine = create_engine(db_uri, convert_unicode=True)
 
 Base = declarative_base()
