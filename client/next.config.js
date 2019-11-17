ce8638845c978255781ca4943a98ef2df67cc406
lightyearsaway <lightyearsaway25@gmail.com>
2019-11-17 12:58:32 -0800

    Start setting up Apollo

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,24 @@
-const withCSS = require('@zeit/next-css');
+const withCSS = require("@zeit/next-css");
+const webpack = require("webpack");
+require("dotenv").config();
+
 module.exports = withCSS({
-  /* config options here */
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader"
+    });
+
+    config.node = {
+      fs: "empty"
+    };
+
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+    config.plugins.push(new webpack.DefinePlugin(env));
+    return config;
+  }
 });
