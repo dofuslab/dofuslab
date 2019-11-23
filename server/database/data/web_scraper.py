b4dbe89ecce8fb27121350c4e7116d03e61793db
Sam <samlee405@yahoo.com>
2019-11-22 17:47:01 -0800

    Adjusted json structure for  stats of items and set bonuses. Added custom stat scraping

--- server/database/data/web_scraper.py
+++ server/database/data/web_scraper.py
@@ -2,45 +2,79 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+
 class ItemScraper:
-    def getItemsFromPage(url):
-        urlResponse = requests.get(url)
-        soup = BeautifulSoup(urlResponse.text, 'html.parser')
-        itemURLs = []
+    def get_items_from_page(url):
+        url_response = requests.get(url)
+        soup = BeautifulSoup(url_response.text, 'html.parser')
+        item_urls = []
 
-        itemTable = soup.find('table', {'class': 'ak-table'}).tbody.find_all('tr')
+        item_table = soup.find('table', {'class': 'ak-table'}).tbody.find_all(
+            'tr'
+        )
 
-        for item in itemTable:
+        for item in item_table:
             item = 'https://www.dofus.com' + item.find('a')['href']
-            itemURLs.append(item)
+            item_urls.append(item)
 
-        return itemURLs
+        return item_urls
 
-    def getItemStats(url):
-        urlResponse = requests.get(url)
-        soup = BeautifulSoup(urlResponse.text, 'html.parser')
+    def get_item_stats(url):
+        url_response = requests.get(url)
+        soup = BeautifulSoup(url_response.text, 'html.parser')
 
         # get and clean data
-        name = soup.find('h1', attrs = {'class': 'ak-return-link'}).text.strip()
-        item_type = soup.find('div', attrs = {'class': 'ak-encyclo-detail-type col-xs-6'}).text[7:]
-        level = soup.find('div', attrs = {'class': 'ak-encyclo-detail-level col-xs-6 text-right'}).text[7:]
-        image = soup.find('img', attrs = {'class': 'img-maxresponsive'})["src"]
-        set = (soup.find('div', attrs = {'class': 'ak-container ak-panel-stack ak-glue'})
-                  .find_all('div', attrs={'class': 'ak-panel-title'})[3]
-                  .find('a')
-        )
+        name = soup.find('h1', attrs={'class': 'ak-return-link'}).text.strip()
+        item_type = soup.find(
+            'div', attrs={'class': 'ak-encyclo-detail-type col-xs-6'}
+        ).text[7:]
+        level = soup.find(
+            'div',
+            attrs={'class': 'ak-encyclo-detail-level col-xs-6 text-right'},
+        ).text[7:]
+        image = soup.find('img', attrs={'class': 'img-maxresponsive'})["src"]
+        set = None
+        try:
+            set = (
+                soup.find(
+                    'div', attrs={'class': 'ak-container ak-panel-stack ak-glue'}
+                )
+                .find_all('div', attrs={'class': 'ak-panel-title'})[3]
+                .find('a')
+            )
+        except IndexError:
+            # no set found for this item
+            pass
+        except:
+            print("Error ocurred")
 
         if set:
             set = set.text
 
         # Retrieve item stats
-        rawStats = soup.find('div', {'class': 'ak-container ak-content-list ak-displaymode-col'})
+        raw_stats = soup.find(
+            'div', {'class': 'ak-container ak-content-list ak-displaymode-col'}
+        )
         stats = []
+        custom_stats = []
 
-        for stat in rawStats:
-            description = stat.find_next('div', {'class': 'ak-title'}).text.strip()
-            type = ""
-            maxStat = ""
+        for stat in raw_stats:
+            description = stat.find_next(
+                'div', {'class': 'ak-title'}
+            ).text.strip()
+
+            # The dofus site does not distinguish special descriptive or custom
+            # stats from normal combat stats. As such, discerning them from
+            # normal stats is a bit difficult. For now, I'm simply using a
+            # char count analysis to separate the 2. This will need testing
+            # or refactoring at a later time
+            if len(description) > 40:
+                custom_stats.append(description)
+                continue
+
+            type = None
+            min_stat = None
+            max_stat = None
 
             # check and adjust for the description typo that substitutes "HP" for "Initiative"
             description = description.replace("HP", "Initiative")
@@ -48,64 +82,91 @@ class ItemScraper:
             # check and adjust for values that have ranges and negative values
             if "to" in description and "-" not in description:
                 arr = description.split(' ')
-                maxStat = int(arr[2].replace('%', ''))
+                min_stat = int(arr[0].replace('%', ''))
+                max_stat = int(arr[2].replace('%', ''))
                 del arr[0]
                 del arr[0]
                 del arr[0]
                 type = ' '.join(arr)
             elif "to" in description and "-" in description:
                 arr = description.split(" ")
-                maxStat = int(arr[0].replace("%", ""))
+                min_stat = int(arr[2].replace('%', ''))
+                max_stat = int(arr[0].replace("%", ""))
                 del arr[0]
                 del arr[0]
                 del arr[0]
                 type = ' '.join(arr)
             else:
                 arr = description.split(' ')
-                maxStat = int(arr[0].replace('%', ''))
+                max_stat = int(arr[0].replace('%', ''))
                 del arr[0]
                 type = ' '.join(arr)
 
             if '%' in description and "Critical" not in description:
                 type = '% ' + type
 
-            stats.append({'stat': type, 'description': description, 'maxStat': maxStat})
+            stats.append(
+                {'stat': type, 'minStat': min_stat, 'maxStat': max_stat}
+            )
 
         # Retrive item conditions
-        rawConditions = soup.find('div', attrs = {'class': 'ak-container ak-panel no-padding'})
+        raw_conditions = soup.find(
+            'div', attrs={'class': 'ak-container ak-panel no-padding'}
+        )
         conditions = {}
 
-        if rawConditions:
-            rawConditions = rawConditions.text.strip().strip("Conditions").strip().split(" ")
-            conditionType = rawConditions[0]
-            conditionLimitType = rawConditions[1]
-            conditionLimit = rawConditions[2]
+        if raw_conditions:
+            raw_conditions = (
+                raw_conditions.text.strip()
+                .strip("Conditions")
+                .strip()
+                .split(" ")
+            )
+            condition_type = raw_conditions[0]
+            condition_limit_type = raw_conditions[1]
+            condition_limit = raw_conditions[2]
 
-            conditions = {"conditionType": conditionType, "conditionLimitType": conditionLimitType, "conditionLimit": conditionLimit}
+            conditions = {
+                "conditionType": condition_type,
+                "conditionLimitType": condition_limit_type,
+                "conditionLimit": condition_limit,
+            }
 
-        item = {'name': name, 'item_type': item_type, 'set': set, 'level': level, 'stats': stats, 'conditions': conditions, 'image_url': image}
+        item = {
+            'name': name,
+            'itemType': item_type,
+            'set': set,
+            'level': level,
+            'stats': stats,
+            'customStats': custom_stats,
+            'conditions': conditions,
+            'image_url': image,
+        }
 
         return item
 
-    def writeToFile(item_data):
+    def write_to_file(item_data):
         with open('items.json', 'w') as outfile:
             json.dump(item_data, outfile)
 
+
 class SetScraper:
-    def getSetsFromPage(url):
+    def get_sets_from_page(url):
         url_response = requests.get(url)
         soup = BeautifulSoup(url_response.text, 'html.parser')
 
         set_urls = []
 
-        set_table = soup.find('table', {'class': 'ak-table'}).tbody.find_all('tr')
+        set_table = soup.find('table', {'class': 'ak-table'}).tbody.find_all(
+            'tr'
+        )
         for set in set_table:
             set = 'https://www.dofus.com' + set.find('a')['href']
             set_urls.append(set)
 
         return set_urls
 
-    def getSetInfo(url):
+    def get_set_info(url):
         url_response = requests.get(url)
         soup = BeautifulSoup(url_response.text, 'html.parser')
 
@@ -113,7 +174,9 @@ class SetScraper:
 
         # extracting items may be extraneous. Review this segment
         item_names = []
-        items = soup.find('div', attrs={'class': 'ak-item-list-preview'}).find_all('a')
+        items = soup.find(
+            'div', attrs={'class': 'ak-item-list-preview'}
+        ).find_all('a')
         for item in items:
             item_name = item['href'].split('-')
             del item_name[0]
@@ -126,11 +189,13 @@ class SetScraper:
         raw_bonuses = soup.find_all('div', attrs={'class': 'set-bonus-list'})
         for i in range(len(raw_bonuses)):
             stats = []
-            bonuses = raw_bonuses[i].find_all('div', attrs={'class': 'ak-title'})
+            bonuses = raw_bonuses[i].find_all(
+                'div', attrs={'class': 'ak-title'}
+            )
             for bonus in bonuses:
                 description = bonus.text.strip()
-                type = ""
-                max_stat = 0
+                type = None
+                value = None
 
                 # check and adjust for the description typo that substitutes "HP" for "Initiative"
                 description = description.replace("HP", "Initiative")
@@ -138,74 +203,86 @@ class SetScraper:
                 # check and adjust for values that have ranges and negative values
                 if "to" in description and "-" not in description:
                     arr = description.split(' ')
-                    maxStat = arr[2].replace('%', '')
+                    max_stat = arr[2].replace('%', '')
                     del arr[0]
                     del arr[0]
                     del arr[0]
                     type = ' '.join(arr)
                 elif "to" in description and "-" in description:
                     arr = description.split(" ")
-                    maxStat = arr[0].replace("%", "")
+                    max_stat = arr[0].replace("%", "")
                     del arr[0]
                     del arr[0]
                     del arr[0]
                     type = ' '.join(arr)
                 else:
                     arr = description.split(' ')
-                    maxStat = arr[0].replace('%', '')
+                    max_stat = arr[0].replace('%', '')
                     del arr[0]
                     type = ' '.join(arr)
 
                 if '%' in description and "Critical" not in description:
                     type = '% ' + type
 
-                stats.append({'stat': type, 'description': description, 'maxStat': maxStat})
+                stats.append({'stat': type, 'value': max_stat})
 
             item_count = 2 + i
-            bonus = {'item_count': item_count, 'stats': stats}
+            bonus = {item_count: stats}
             all_bonuses.append(bonus)
 
         set = {'name': name, 'items': item_names, 'bonuses': all_bonuses}
 
         return set
 
-    def writeToFile(set_data):
+    def write_to_file(set_data):
+        print("writing set data to file")
         with open('sets.json', 'w') as outfile:
             json.dump(set_data, outfile)
 
+
 class ClassScraper:
-    def getClassesFromPage():
-        url_response = requests.get('https://www.dofus.com/en/mmorpg/encyclopedia/classes')
+    def get_classes_from_page():
+        url_response = requests.get(
+            'https://www.dofus.com/en/mmorpg/encyclopedia/classes'
+        )
         soup = BeautifulSoup(url_response.text, 'html.parser')
 
         class_urls = []
 
-        class_table = (soup.find('div', attrs={'class': 'ak-content-sections'})
-                           .find('div', attrs={'class': 'row'})
-                           .find_all('div', attrs={'class': 'col-sm-6'})
+        class_table = (
+            soup.find('div', attrs={'class': 'ak-content-sections'})
+            .find('div', attrs={'class': 'row'})
+            .find_all('div', attrs={'class': 'col-sm-6'})
         )
 
         for some_class in class_table:
-            class_urls.append('https://www.dofus.com' + some_class.find('div', attrs={'class': 'ak-breed-section'}).a['href'])
+            class_urls.append(
+                'https://www.dofus.com'
+                + some_class.find('div', attrs={'class': 'ak-breed-section'}).a[
+                    'href'
+                ]
+            )
 
         return class_urls
 
-    def getClassInfo(url):
-        urlResponse = requests.get(url)
-        soup = BeautifulSoup(urlResponse.text, 'html.parser')
+    def get_class_info(url):
+        url_response = requests.get(url)
+        soup = BeautifulSoup(url_response.text, 'html.parser')
 
         name = ''.join(url.split('-')[-1:]).capitalize()
         spells = []
 
         # get spell urls from class page
         spell_urls = []
-        raw_spells = (soup.find('div', attrs={'class': 'ak-spell-list-row'})
-                          .find_all('div', attrs={'class': 'ak-spell-group'})
-        )
+        raw_spells = soup.find(
+            'div', attrs={'class': 'ak-spell-list-row'}
+        ).find_all('div', attrs={'class': 'ak-spell-group'})
 
         for spell in raw_spells:
             variant_urls = []
-            raw_variants = spell.find_all('div', attrs={'class': 'ak-list-block'})
+            raw_variants = spell.find_all(
+                'div', attrs={'class': 'ak-list-block'}
+            )
 
             for variant in raw_variants:
                 variant_urls.append(variant.a['href'])
@@ -214,42 +291,47 @@ class ClassScraper:
 
         # get spell data
 
-
         return {'name': name, 'spells': spells}
+
 
 def __main__():
     items = []
-    itemData = []
+    item_data = []
 
     # get all item urls
     for i in range(1, 2):
-        url = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment?size=24&page=' + str(i)
+        url = (
+            'https://www.dofus.com/en/mmorpg/encyclopedia/equipment?size=24&page='
+            + str(i)
+        )
         items = items + ItemScraper.getItemsFromPage(url)
 
     # get item data from each url
     for url in items:
-        item = ItemScraper.getItemStats(url)
-        itemData.append(item)
+        item = ItemScraper.get_item_stats(url)
+        item_data.append(item)
 
-    writeToFile(itemData)
+    write_to_file(item_data)
+
 
 # ClassScraper.getClassUrls('https://www.dofus.com/en/mmorpg/encyclopedia/classes/6-ecaflip')
+
 
 items = []
 a = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment/14085-sinistrofu-cloak'
 b = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment/14086-sinistrofu-amulet'
 c = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment/14087-sinistrofu-boots'
-items.append(ItemScraper.getItemStats(a))
-items.append(ItemScraper.getItemStats(b))
-items.append(ItemScraper.getItemStats(c))
-ItemScraper.writeToFile(items)
+items.append(ItemScraper.get_item_stats(a))
+items.append(ItemScraper.get_item_stats(b))
+items.append(ItemScraper.get_item_stats(c))
+ItemScraper.write_to_file(items)
 
 # items = []
-# item_test = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment/15699-belteen'
-# items.append(ItemScraper.getItemStats(item_test))
-# ItemScraper.writeToFile(items)
+# item_test = 'https://www.dofus.com/en/mmorpg/encyclopedia/equipment/20358-death-defying'
+# items.append(ItemScraper.get_item_stats(item_test))
+# ItemScraper.write_to_file(items)
 
 # sets = []
 # url = 'https://www.dofus.com/en/mmorpg/encyclopedia/sets/275-sinistrofu-set'
-# sets.append(SetScraper.getSetInfo(url))
-# SetScraper.writeToFile(sets)
+# sets.append(SetScraper.get_set_info(url))
+# SetScraper.write_to_file(sets)
