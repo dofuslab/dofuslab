37e0101c2cc9327a6989d745212dc3535bf40cf8
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-03-12 16:48:40 -0700

    Deployment fixes

--- client/server/index.ts
+++ client/server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import next from 'next';
 import nextI18NextMiddleware from 'next-i18next/middleware';
-import nextI18next from './i18n';
+import nextI18next from '../i18n';
 
 const port = process.env.PORT || 3000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
