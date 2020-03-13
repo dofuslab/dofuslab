61a1f715ea98e51e8836691b037691783957b640
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-03-12 23:15:48 -0700

    Add image scripts

--- server/download_images.py
+++ server/download_images.py
@@ -17,7 +17,8 @@ for num, (image_url,) in enumerate(image_urls, start=1):
         print("downloaded", num, "/", len(image_urls))
         filename = re.search("\d+\.png", image_url)[0]
         path = os.path.join(folder, filename)
-        with open(path, "wb") as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-            print("wrote to", path)
+        print(path)
+        # with open(path, "wb") as f:
+        #     r.raw.decode_content = True
+        #     shutil.copyfileobj(r.raw, f)
+        #     print("wrote to", path)
