396715bf354647772fee1a077387183f39f1d957
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-03-14 12:53:41 -0700

    pm2 deployment scripts

---
+++ client/ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: 'yarn start',
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'jeremy',
+      host: '134.209.168.215',
+      ref: 'origin/master',
+      repo: 'git@github.com:samlee405/dofus-lab.git',
+      path: '~/dofus-lab/client',
+      'post-deploy':
+        'yarn && pm2 reload ecosystem.config.js --env production && pm2 save',
+    },
+  },
+};
