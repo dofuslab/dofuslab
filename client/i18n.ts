c382379374bb723c5a973bcd307be873588b054e
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-04-10 22:40:35 -0700

    Fix client i18n

--- client/i18n.ts
+++ client/i18n.ts
@@ -3,7 +3,8 @@ import NextI18Next from 'next-i18next';
 const DEFAULT_LANGUAGE = 'en';
 // const OTHER_LANGUAGES = ['fr', 'de', 'pt', 'it', 'es'] as const;
 
-export const LANGUAGES = [DEFAULT_LANGUAGE, ...OTHER_LANGUAGES] as const;
+// export const LANGUAGES = [DEFAULT_LANGUAGE, ...OTHER_LANGUAGES] as const;
+export const LANGUAGES = [DEFAULT_LANGUAGE] as const;
 
 export type TLanguage = typeof LANGUAGES[number];
 
@@ -17,16 +18,16 @@ export const langToFullName = (language: TLanguage) => {
   switch (language) {
     case 'en':
       return 'English';
-    case 'fr':
-      return 'Français';
-    case 'de':
-      return 'Deutsch';
-    case 'pt':
-      return 'Português';
-    case 'it':
-      return 'Italiano';
-    case 'es':
-      return 'Español';
+    // case 'fr':
+    //   return 'Français';
+    // case 'de':
+    //   return 'Deutsch';
+    // case 'pt':
+    //   return 'Português';
+    // case 'it':
+    //   return 'Italiano';
+    // case 'es':
+    //   return 'Español';
   }
 };
 
