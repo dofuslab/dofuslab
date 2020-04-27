d1470eea64ec9ddcf5529b236a9390785e807b50
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-04-27 11:21:46 -0700

    Try adding more functions to sync_item

--- server/oneoff/sync_item.py
+++ server/oneoff/sync_item.py
@@ -3,13 +3,44 @@ import os
 from app import session_scope
 from app.database.model_item import ModelItem
 from app.database.model_item_stat import ModelItemStat
+from app.database.model_item_stat_translation import ModelItemStatTranslation
 from app.database.model_item_translation import ModelItemTranslation
 from app.database.model_item_type import ModelItemType
 from oneoff.database_setup import to_stat_enum
 
 app_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-allowed_file_names = ["items", "mounts", "pets"]
+allowed_file_names = ["items", "mounts", "pets", "prysmaradites"]
+languages = ["en", "fr", "pt", "it", "es", "de"]
+
+
+def update_or_create_item(db_session, item_name, record):
+    print(item_name)
+    translations = (
+        db_session.query(ModelItemTranslation)
+        .filter(
+            ModelItemTranslation.locale == "en", ModelItemTranslation.name == item_name,
+        )
+        .all()
+    )
+    print(translations)
+    if len(translations) > 1:
+        print("Error: Multiple items with that name exist in the database")
+    elif len(translations) == 1:
+        print("Item already exists in database. Updating item...")
+        item = translations[0].item
+        create_item_translations(db_session, record, item)
+        print("Item translations successfully updated")
+        db_session.query(ModelItemStat).filter_by(item_id=item.uuid).delete()
+        create_item_stats(db_session, record, item)
+        print("Item stats successfully updated")
+    else:
+        should_create_response = input(
+            "Item does not exist in database. Would you like to create it? (Y/n): "
+        )
+        if should_create_response == "Y":
+            create_item(db_session, record)
+            print("Item successfully created")
 
 
 def create_item(db_session, record):
@@ -43,7 +74,8 @@ def create_item(db_session, record):
 
 def create_item_stats(db_session, record, item):
     i = 0
-    for stat in record["stats"]:
+
+    for stat in record.get("stats", []):
         item_stat = ModelItemStat(
             item_id=item.uuid,
             stat=to_stat_enum[stat["stat"]],
@@ -53,6 +85,31 @@ def create_item_stats(db_session, record, item):
         )
         db_session.add(item_stat)
         i = i + 1
+    if record["customStats"]:
+        num_of_stats = len(record["customStats"]["en"])
+
+        for j in range(num_of_stats):
+            item_stat = ModelItemStat(order=i + j)
+            for locale in record["customStats"]:
+                custom_stat = record["customStats"][locale][j]
+                stat_translation = ModelItemStatTranslation(
+                    item_stat_id=item_stat.uuid, locale=locale, custom_stat=custom_stat,
+                )
+                db_session.add(stat_translation)
+            db_session.add(item_stat)
+
+
+def create_item_translations(db_session, record, item):
+    for locale in languages:
+        if record["name"][locale] and not (
+            db_session.query(ModelItemTranslation)
+            .filter_by(item_id=item.uuid, locale=locale, name=record["name"][locale])
+            .first()
+        ):
+            item_translation = ModelItemTranslation(
+                item_id=item.uuid, locale=locale, name=record["name"][locale]
+            )
+            db_session.add(item_translation)
 
 
 def sync_item():
@@ -80,38 +137,46 @@ def sync_item():
 
         should_prompt_item = True
         while should_prompt_item:
-            item_name = input("Enter item name, e.g. 'Yellow Piwin': ")
-            if item_name in name_to_record_map:
-                record = name_to_record_map[item_name]
-                should_prompt_item = False
-                with session_scope() as db_session:
-                    translations = (
-                        db_session.query(ModelItemTranslation)
-                        .filter(
-                            ModelItemTranslation.locale == "en",
-                            ModelItemTranslation.name == item_name,
-                        )
-                        .all()
-                    )
-                    if len(translations) > 1:
-                        print(
-                            "Error: Multiple items with that name exist in the database"
-                        )
-                    elif len(translations) == 1:
-                        print("Item already exists in database. Updating stats...")
-                        item = translations[0].item
-                        db_session.query(ModelItemStat).filter_by(
-                            item_id=item.uuid
-                        ).delete()
-                        create_item_stats(db_session, record, item)
-                        print("Item stats successfully updated")
-                    else:
-                        should_create_response = input(
-                            "Item does not exist in database. Would you like to create it? (Y/n): "
-                        )
-                        if should_create_response == "Y":
-                            create_item(db_session, record)
-                            print("Item successfully created")
+            item_name = input(
+                "Enter item name, e.g. 'Yellow Piwin', or type 'update all' to update all items in file: "
+            )
+            with session_scope() as db_session:
+                if item_name == "update all":
+                    for record in data:
+                        update_or_create_item(db_session, record["name"]["en"], record)
+                elif item_name in name_to_record_map:
+                    record = name_to_record_map[item_name]
+                    should_prompt_item = False
+                    update_or_create_item(db_session, item_name, record)
+                    # translations = (
+                    #     db_session.query(ModelItemTranslation)
+                    #     .filter(
+                    #         ModelItemTranslation.locale == "en",
+                    #         ModelItemTranslation.name == item_name,
+                    #     )
+                    #     .all()
+                    # )
+                    # if len(translations) > 1:
+                    #     print(
+                    #         "Error: Multiple items with that name exist in the database"
+                    #     )
+                    # elif len(translations) == 1:
+                    #     print("Item already exists in database. Updating item...")
+                    #     item = translations[0].item
+                    #     create_item_translations(db_session, record, item)
+                    #     print("Item translations successfully updated")
+                    #     db_session.query(ModelItemStat).filter_by(
+                    #         item_id=item.uuid
+                    #     ).delete()
+                    #     create_item_stats(db_session, record, item)
+                    #     print("Item stats successfully updated")
+                    # else:
+                    #     should_create_response = input(
+                    #         "Item does not exist in database. Would you like to create it? (Y/n): "
+                    #     )
+                    #     if should_create_response == "Y":
+                    #         create_item(db_session, record)
+                    #         print("Item successfully created")
 
 
 if __name__ == "__main__":
