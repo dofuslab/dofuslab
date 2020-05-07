d93aca2da310dae9e91784243d5efb25f3561df8
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-05-07 03:59:43 -0700

    :Add protocol to share link

--- client/components/common/BuildActions.tsx
+++ client/components/common/BuildActions.tsx
@@ -93,7 +93,7 @@ const BuildActions: React.FC<Props> = ({ customSet, isMobile, isClassic }) => {
   const linkTextareaRef = React.useRef<HTMLTextAreaElement | null>(null);
 
   const onCopyLink = async () => {
-    let url = `${window.location.host}/view/${customSet.id}/`;
+    let url = `${window.location.protocol}//${window.location.host}/view/${customSet.id}/`;
     if (router.query.class) {
       const singleClass = Array.isArray(router.query.class)
         ? router.query.class[0]
