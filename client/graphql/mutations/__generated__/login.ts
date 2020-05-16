69e65df8a1ad03a910e3f28e0ffa43fde20e5dde
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2020-05-16 04:18:46 -0700

    Fix type errors, edit translations

--- client/graphql/mutations/__generated__/login.ts
+++ client/graphql/mutations/__generated__/login.ts
@@ -3,20 +3,95 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
+import { Stat, WeaponEffectType } from './../../../__generated__/globalTypes';
+
 // ====================================================
 // GraphQL mutation operation: login
 // ====================================================
 
-export interface login_loginUser_user {
-  __typename: "User";
+export interface login_loginUser_user_favoriteItems_stats {
+  __typename: 'ItemStat';
   id: any;
+  order: number;
+  maxValue: number | null;
+  stat: Stat | null;
+  customStat: string | null;
+}
+
+export interface login_loginUser_user_favoriteItems_weaponStats_weaponEffects {
+  __typename: 'WeaponEffect';
+  id: any;
+  minDamage: number | null;
+  maxDamage: number;
+  effectType: WeaponEffectType;
+}
+
+export interface login_loginUser_user_favoriteItems_weaponStats {
+  __typename: 'WeaponStat';
+  id: any;
+  apCost: number;
+  usesPerTurn: number;
+  minRange: number | null;
+  maxRange: number;
+  baseCritChance: number | null;
+  critBonusDamage: number | null;
+  weaponEffects: login_loginUser_user_favoriteItems_weaponStats_weaponEffects[];
+}
+
+export interface login_loginUser_user_favoriteItems_itemType_eligibleItemSlots {
+  __typename: 'ItemSlot';
+  id: any;
+  order: number;
+}
+
+export interface login_loginUser_user_favoriteItems_itemType {
+  __typename: 'ItemType';
+  id: any;
+  name: string;
+  enName: string;
+  eligibleItemSlots: login_loginUser_user_favoriteItems_itemType_eligibleItemSlots[];
+}
+
+export interface login_loginUser_user_favoriteItems_set_bonuses {
+  __typename: 'SetBonus';
+  id: any;
+  numItems: number;
+  stat: Stat | null;
+  value: number | null;
+  customStat: string | null;
+}
+
+export interface login_loginUser_user_favoriteItems_set {
+  __typename: 'Set';
+  id: any;
+  name: string;
+  bonuses: login_loginUser_user_favoriteItems_set_bonuses[];
+}
+
+export interface login_loginUser_user_favoriteItems {
+  __typename: 'Item';
+  id: any;
+  name: string;
+  level: number;
+  imageUrl: string;
+  stats: login_loginUser_user_favoriteItems_stats[];
+  weaponStats: login_loginUser_user_favoriteItems_weaponStats | null;
+  conditions: any | null;
+  itemType: login_loginUser_user_favoriteItems_itemType;
+  set: login_loginUser_user_favoriteItems_set | null;
+}
+
+export interface login_loginUser_user {
+  __typename: 'User';
+  id: any;
+  favoriteItems: login_loginUser_user_favoriteItems[];
   username: string;
   email: string;
   verified: boolean;
 }
 
 export interface login_loginUser {
-  __typename: "LoginUser";
+  __typename: 'LoginUser';
   user: login_loginUser_user | null;
 }
 
