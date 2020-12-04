edf200245880761913b1473e5a00a45b744e977b
Luka Kusulja <11875515+luka-kusulja@users.noreply.github.com>
2020-12-05 00:57:55 +0100

    initial commit

---
+++ server/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+psql -v ON_ERROR_STOP=1 --host "postgres" --username "dofuslab" --dbname "dofuslab" <<-EOSQL
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+EOSQL
+
+flask db upgrade
+cd dofuslab-server
+printf 'y\n' | python -m oneoff.database_setup
+printf 'y\n' | python -m oneoff.update_image_urls
+cd ..
+
+flask run --host=0.0.0.0 --port=5501
