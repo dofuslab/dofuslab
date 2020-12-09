b9463be2a94266c88c8b92b295133ac0109c0d2e
Luka Kusulja <11875515+luka-kusulja@users.noreply.github.com>
2020-12-09 23:58:53 +0100

    initdb script removed use docker-compose instead

--- initdb.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --rm -f "server\Dockerfile.initdb" -t dofuslab_init:latest "server"
-docker run --network dofuslab_default --name dofuslab_init_1 dofuslab_init
-docker stop dofuslab_init_1
-docker rm dofuslab_init_1
