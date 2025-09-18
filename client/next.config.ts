10a95077d9e89b1f76d8888c152ee17ff7ba2f0c
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2025-09-17 23:19:00 -0700

    more memory optimizations

--- client/next.config.ts
+++ client/next.config.ts
@@ -48,7 +48,21 @@ const nextConfig: NextConfig = withBundleAnalyzer({
   experimental: {
     webpackMemoryOptimizations: true,
     webpackBuildWorker: true,
+    serverSourceMaps: false,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: false,
 });
 
 export default nextConfig;
