7e7134d52e7b9fafd5257f1eaab1e0b520fd26e3
lightyearsaway <27422540+lightyearsaway@users.noreply.github.com>
2026-07-09 08:35:47 -0700

    Pin expensive build regression benchmarks

--- server/scripts/test_build_discovery_expensive_regressions.py
+++ server/scripts/test_build_discovery_expensive_regressions.py
@@ -31,17 +31,24 @@ class BuildDiscoveryExpensiveRegressionTest(unittest.TestCase):
         *,
         min_primary_stat: int,
         primary_stat: str,
+        min_score: float,
+        expected_item_ids: set,
         required_item_id: Optional[str] = None,
     ):
         builds = response["builds"]
         self.assertGreater(len(builds), 0)
 
         best = builds[0]
+        self.assertGreaterEqual(best["score"], min_score)
         self.assertGreaterEqual(best["totals"]["AP"], 11)
         self.assertGreaterEqual(best["totals"]["MP"], 6)
         self.assertGreaterEqual(best["totals"][primary_stat], min_primary_stat)
         self.assertEqual(best["exos"], {})
         self.assertEqual(best["conditionFailures"], [])
+        self.assertEqual(
+            {item["id"] for item in best["items"].values()},
+            expected_item_ids,
+        )
 
         if required_item_id:
             self.assertIn(
@@ -67,6 +74,25 @@ class BuildDiscoveryExpensiveRegressionTest(unittest.TestCase):
             response,
             primary_stat="Strength",
             min_primary_stat=1200,
+            min_score=2140.39,
+            expected_item_ids={
+                "31789",
+                "19596",
+                "31787",
+                "30858",
+                "14077",
+                "17998",
+                "19245",
+                "14076",
+                "14168",
+                "33044",
+                "16196",
+                "16193",
+                "16254",
+                "16332",
+                "16333",
+                "16335",
+            },
         )
 
     def test_tier_one_chance_iop_11_6_no_exo_still_generates(self):
@@ -87,6 +113,25 @@ class BuildDiscoveryExpensiveRegressionTest(unittest.TestCase):
             response,
             primary_stat="Chance",
             min_primary_stat=800,
+            min_score=2006.29,
+            expected_item_ids={
+                "33608",
+                "15699",
+                "33607",
+                "18715",
+                "19985",
+                "22186",
+                "22187",
+                "15694",
+                "18591",
+                "33295",
+                "13783",
+                "12716",
+                "12690",
+                "12726",
+                "13768",
+                "13830",
+            },
         )
 
     def test_tier_one_chance_iop_locked_shaker_still_generates(self):
@@ -108,6 +153,25 @@ class BuildDiscoveryExpensiveRegressionTest(unittest.TestCase):
             response,
             primary_stat="Chance",
             min_primary_stat=800,
+            min_score=2017.2,
+            expected_item_ids={
+                "16333",
+                "14094",
+                "15699",
+                "31787",
+                "18715",
+                "19985",
+                "18029",
+                "18030",
+                "15694",
+                "18591",
+                "33295",
+                "16248",
+                "16193",
+                "16264",
+                "16332",
+                "16335",
+            },
             required_item_id=SHAKER_TROPHY_ID,
         )
 
